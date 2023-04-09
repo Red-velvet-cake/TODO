7ccf9f9e210ab92ac91cf47b934808fe8f218ecc
Sajadio <sjd04969@gmail.com>
2023-04-09 08:02:29 +0300

    add base fragment and restructuring the project's packages

---
+++ app/src/main/java/com/red_velvet_cake/dailytodo/ui/base/BaseFragment.kt
@@ -0,0 +1,36 @@
+package com.red_velvet_cake.dailytodo.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+
+    private var _binding: VB? = null
+    abstract val inflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
+
+    protected val binding: VB
+        get() = _binding as VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflater(inflater, container, false)
+        return _binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUp()
+        addCallBacks()
+    }
+
+    abstract fun setUp()
+
+    abstract fun addCallBacks()
+}
