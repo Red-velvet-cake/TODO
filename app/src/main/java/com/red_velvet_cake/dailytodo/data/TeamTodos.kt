9c6c9c21ee8657c53f8c7990952fb70a45d01c7b
jalal cheff <jlalalmyahy571@gmail.com>
2023-04-12 12:58:46 +0300

    parse data from api using gson library

---
+++ app/src/main/java/com/red_velvet_cake/dailytodo/data/TeamTodos.kt
@@ -0,0 +1,7 @@
+package com.red_velvet_cake.dailytodo.data
+
+data class TeamTodos(
+    val value:List<TeamTodo>,
+    val message:String?,
+    val isSuccess:Boolean
+)
--- app/src/main/java/com/red_velvet_cake/dailytodo/data/TodoServiceImpl.kt
+++ app/src/main/java/com/red_velvet_cake/dailytodo/data/TodoServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.red_velvet_cake.dailytodo.data
 
 import android.util.Log
+import com.google.gson.Gson
 import com.red_velvet_cake.dailytodo.domain.TodoService
 import okhttp3.*
 import java.io.IOException
@@ -9,20 +10,28 @@ class TodoServiceImpl: TodoService {
     private val client= OkHttpClient()
     override fun getTeamToDo() {
          val token="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwczovL3RoZS1jaGFuY2Uub3JnLyIsInN1YiI6IjY3NzVlYWJhLTEwMzktNGViMi05ODA5LTI5MjUzNWRmMDM1MCIsInRlYW1JZCI6IjAxYThhOTg4LTQ0NjItNDNhNi1hOThhLTE2MjY4NzNmYTc4NyIsImlzcyI6Imh0dHBzOi8vdGhlLWNoYW5jZS5vcmcvIiwiZXhwIjoxNjgxMzAyMzM4fQ.9N6NeI1h854Ga3tVjphbU1n-fvQayyv64hRk2QUlmBQ"
+         val pathSegments= listOf("todo","team")
+        val scheme="https"
             val url= HttpUrl.Builder().
-            scheme("https").
-            host("team-todo-62dmq.ondigitalocean.app")
-                .addPathSegment("todo")
-                .addPathSegment("team").build()
-            val request= Request.Builder().url(url).addHeader("Authorization",token).build()
+            scheme(scheme).
+            host(HOST_NAME)
+                .addPathSegment(pathSegments[0])
+                .addPathSegment(pathSegments[1]).build()
+            val request= Request.Builder().url(url).addHeader(AUTH,token).build()
             client.newCall(request).enqueue(object : Callback{
                 override fun onFailure(call: Call, e: IOException) {
                     Log.i("token",e.message.toString())
                 }
                 override fun onResponse(call: Call, response: Response) {
-                  Log.i("token",response.body?.string().toString())
+
+                 val result=Gson().fromJson(response.body?.string().toString(),TeamTodos::class.java)
+                    Log.i("token",result.value[0].description)
                 }
             })
 
+    }
+    companion object{
+        const val HOST_NAME="team-todo-62dmq.ondigitalocean.app"
+        const val AUTH="Authorization"
     }
     }
