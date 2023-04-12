7aa7d7173a5050166493fbc96e641762cb6a1722
YaseenOmar <yz456359@gmail.com>
2023-04-12 11:17:00 +0200

    create modle todo and parse with GSON

--- app/src/main/java/com/red_velvet_cake/dailytodo/data/TodoServiceImpl.kt
+++ app/src/main/java/com/red_velvet_cake/dailytodo/data/TodoServiceImpl.kt
@@ -7,18 +7,18 @@ import java.io.IOException
 
 class TodoServiceImpl: TodoService {
     // token just test
-     val TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwczovL3RoZS1jaGFuY2Uub3JnLyIsInN1YiI6IjRmODg2NTIzLTUwNWItNDEzMy05NjEzLWYxZTA1ODVhMzFiOCIsInRlYW1JZCI6IjAxYThhOTg4LTQ0NjItNDNhNi1hOThhLTE2MjY4NzNmYTc4NyIsImlzcyI6Imh0dHBzOi8vdGhlLWNoYW5jZS5vcmcvIiwiZXhwIjoxNjgxMjM4Njc4fQ.D5DB1QmEo7ELXB8exmOSj9xYjEQi7fDCTti-__3BUuk"
+    val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwczovL3RoZS1jaGFuY2Uub3JnLyIsInN1YiI6IjJkZTExZTYyLWQ3ZDQtNGQzZS04ODlhLWQ0YjlkN2RmMjBmMSIsInRlYW1JZCI6IjAxYThhOTg4LTQ0NjItNDNhNi1hOThhLTE2MjY4NzNmYTc4NyIsImlzcyI6Imh0dHBzOi8vdGhlLWNoYW5jZS5vcmcvIiwiZXhwIjoxNjgxMzgyNjQzfQ.VQGGXmQv0OgvIlv35Mv6aLTrK59KZmlAaRvR9rYy2yQ"
     val client = OkHttpClient()
     override fun getAllPersonalTodos(){
         Log.i("iii" , "make request")
-        val request = Request.Builder().url("https://team-todo-62dmq.ondigitalocean.app/todo/personal").header("Authorization","Bearer $TOKEN" ) .build()
+        val request = Request.Builder().url("https://team-todo-62dmq.ondigitalocean.app/todo/personal").header("Authorization","Bearer $token" ) .build()
         client.newCall(request).enqueue(object :Callback{
             override fun onFailure(call: Call, e: IOException) {
                 Log.i("iii" , "fail ${e.message}")
 
             }
             override fun onResponse(call: Call, response: Response) {
-                Log.i("iii" , response.body?.string().toString())
+                Log.i("iii" , " response ${response.body?.string().toString()}")
             }
         })
     }
