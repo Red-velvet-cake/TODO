63a45911680f0964e1e823e83beb3d0eab55743b
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-04-13 17:53:06 +0300

    create register account models

--- app/src/main/java/com/red_velvet_cake/dailytodo/data/remote/dto/ApiResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.red_velvet_cake.dailytodo.data.remote.dto
-
-data class ApiResponse<T>(
-    val value: T?,
-    val message: String,
-    val isSuccess: Boolean
-)
--- app/src/main/java/com/red_velvet_cake/dailytodo/data/remote/dto/RegisterDto.kt
+++
@@ -1,6 +0,0 @@
-package com.red_velvet_cake.dailytodo.data.remote.dto
-
-data class RegisterDto(
-    val userId: String,
-    val username: String,
-)
