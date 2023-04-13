b8430378d63fa636cd52e38dd231cd8521186fc0
hawraamahmoud <hawraaalobaidy2000@gmail.com>
2023-04-13 22:28:14 +0300

    create connection API

--- app/src/main/java/com/red_velvet_cake/dailytodo/domain/TodoService.kt
+++ app/src/main/java/com/red_velvet_cake/dailytodo/domain/TodoService.kt
@@ -1,7 +1,15 @@
 package com.red_velvet_cake.dailytodo.domain
 
 import android.icu.text.CaseMap.Title
+import okhttp3.Response
+import java.io.IOException
 
 interface TodoService {
-    fun creatTeamTodo(title: String,description:String,assignee:String)
+    fun createTeamTodo(
+        title: String,
+        description:String,
+        assignee:String,
+        onCreateTeamTodoSuccess: (Response) -> Unit,
+        onCreateTeamTodoFailure: (IOException) -> Unit
+        )
 }
