b262bffbf2b939bfbf0f5d9714e3cd0dbb2d1983
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-04-15 18:51:49 +0300

    bind register ui

--- app/src/main/java/com/red_velvet_cake/dailytodo/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/red_velvet_cake/dailytodo/ui/register/RegisterFragment.kt
@@ -2,7 +2,9 @@ package com.red_velvet_cake.dailytodo.ui.register
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import com.red_velvet_cake.dailytodo.BuildConfig
+import com.red_velvet_cake.dailytodo.R
 import com.red_velvet_cake.dailytodo.data.model.RegisterAccountResponse
 import com.red_velvet_cake.dailytodo.databinding.FragmentRegisterBinding
 import com.red_velvet_cake.dailytodo.ui.base.BaseFragment
@@ -11,67 +13,119 @@ import okio.IOException
 class RegisterFragment : BaseFragment<FragmentRegisterBinding>(), RegisterView {
     private val presenter = RegisterPresenter(this)
 
-    companion object {
-        const val TAG = "RegisterFragment"
-    }
-
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentRegisterBinding
         get() = FragmentRegisterBinding::inflate
 
-    override fun setUp() {
-        presenter.registerAccount(
-            "uniqueUsername3",
-            "strongPassword",
-            BuildConfig.TEAM_ID
-        )
+    override fun setUp() {}
+
+    override fun addCallBacks() {
+        binding.buttonRegister.setOnClickListener {
+            handleRegisterButtonClick()
+        }
+
+        binding.textViewLogin.setOnClickListener {
+            handleLoginButtonClick()
+        }
     }
 
-    override fun addCallBacks() {}
     override fun handleRegisterAccountSuccess(registerAccountResponse: RegisterAccountResponse) {
-        TODO("Not yet implemented")
+        hideLoading()
+        if (registerAccountResponse.isSuccess) {
+            showToast(getString(R.string.register_success))
+        } else {
+            showToast(registerAccountResponse.message)
+        }
     }
 
     override fun handleRegisterAccountFailure(exception: IOException) {
-        TODO("Not yet implemented")
+        showToast(exception.message.toString())
     }
 
     override fun handleRegisterButtonClick() {
-        TODO("Not yet implemented")
+        if (validateForm()) {
+            showLoading()
+            val username = binding.editTextUsername.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+            presenter.registerAccount(username, password, BuildConfig.TEAM_ID)
+        }
     }
 
-    override fun handleLoginButtonClick() {
-        TODO("Not yet implemented")
+    private fun validateForm(): Boolean {
+        val username = binding.editTextUsername.text.toString().trim()
+        val password = binding.editTextPassword.text.toString().trim()
+        val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
+
+        when {
+            !validateUsername(username) -> {
+                showUsernameValidationError(getString(R.string.username_validation_error))
+                return false
+            }
+
+            !validatePassword(password) -> {
+                showPasswordValidationError(getString(R.string.password_validation_error))
+                return false
+            }
+
+            !validateConfirmPassword(password, confirmPassword) -> {
+                showConfirmPasswordValidationError(getString(R.string.confirm_password_validation_error))
+                return false
+            }
+
+            else -> return true
+        }
     }
+
+    override fun handleLoginButtonClick() {}
 
     override fun validateUsername(username: String): Boolean {
-        TODO("Not yet implemented")
+        return username.length >= 4
     }
 
     override fun validatePassword(password: String): Boolean {
-        TODO("Not yet implemented")
+        return password.length >= 6
     }
 
     override fun validateConfirmPassword(password: String, confirmPassword: String): Boolean {
-        TODO("Not yet implemented")
+        return password == confirmPassword
     }
 
     override fun showUsernameValidationError(message: String) {
-        TODO("Not yet implemented")
+        binding.editTextUsername.error = message
     }
 
     override fun showPasswordValidationError(message: String) {
-        TODO("Not yet implemented")
+        binding.editTextPassword.error = message
     }
 
     override fun showConfirmPasswordValidationError(message: String) {
-        TODO("Not yet implemented")
+        binding.editTextConfirmPassword.error = message
     }
 
     override fun showLoading() {
-        TODO("Not yet implemented")
+        activity?.runOnUiThread {
+            binding.buttonRegister.apply {
+                isEnabled = false
+                text = getString(R.string.loading)
+            }
+        }
     }
 
     override fun hideLoading() {
-        TODO("Not yet implemented")
+        activity?.runOnUiThread {
+            binding.buttonRegister.apply {
+                isEnabled = true
+                text = getString(R.string.register)
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        activity?.runOnUiThread {
+            Toast.makeText(
+                activity,
+                message,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
