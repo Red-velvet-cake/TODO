17fea2a2d9e0bfd84d08aed8e581439e07e3d9c1
loay al shobaki <wowo5xc@gmail.com>
2023-04-15 13:51:18 +0200

    add motion layout

---
+++ app/src/main/java/com/red_velvet_cake/dailytodo/ui/Personaldetails/PersonalTodoDetailsFragment.kt
@@ -0,0 +1,27 @@
+package com.red_velvet_cake.dailytodo.ui.Personaldetails
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.red_velvet_cake.dailytodo.R
+
+
+class PersonalTodoDetailsFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_personal_todo_details, container, false)
+    }
+
+}
