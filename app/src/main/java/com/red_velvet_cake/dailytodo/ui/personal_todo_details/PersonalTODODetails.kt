c1802c1b66b6bc6209bf4f51667ef96aa3cbf661
Sajadio <sjd04969@gmail.com>
2023-04-17 23:00:35 +0300

    create personal todo details fragment

---
+++ app/src/main/java/com/red_velvet_cake/dailytodo/ui/personal_todo_details/PersonalTODODetails.kt
@@ -0,0 +1,20 @@
+package com.red_velvet_cake.dailytodo.ui.personal_todo_details
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.red_velvet_cake.dailytodo.databinding.FragmentPersonalTodoDetailsBinding
+import com.red_velvet_cake.dailytodo.ui.base.BaseFragment
+
+class PersonalTODODetails: BaseFragment<FragmentPersonalTodoDetailsBinding>() {
+
+    override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentPersonalTodoDetailsBinding
+        get() = FragmentPersonalTodoDetailsBinding::inflate
+
+    override fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    override fun addCallBacks() {
+        TODO("Not yet implemented")
+    }
+}
