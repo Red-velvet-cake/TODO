1bb3b631428df8117e1d5d4a64d22ac4da171b23
Sajadio <sjd04969@gmail.com>
2023-04-18 00:18:26 +0300

    create newInstance function to pass data

--- app/src/main/java/com/red_velvet_cake/dailytodo/ui/personal_todo_details/PersonalTODODetails.kt
+++ app/src/main/java/com/red_velvet_cake/dailytodo/ui/personal_todo_details/PersonalTODODetails.kt
@@ -1,20 +1,39 @@
 package com.red_velvet_cake.dailytodo.ui.personal_todo_details
 
+import android.R.attr.defaultValue
+import android.R.attr.key
+import android.os.Build
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import com.red_velvet_cake.dailytodo.data.model.PersonalTodo
 import com.red_velvet_cake.dailytodo.databinding.FragmentPersonalTodoDetailsBinding
 import com.red_velvet_cake.dailytodo.ui.base.BaseFragment
 
-class PersonalTODODetails: BaseFragment<FragmentPersonalTodoDetailsBinding>() {
+
+class PersonalTODODetails : BaseFragment<FragmentPersonalTodoDetailsBinding>() {
 
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentPersonalTodoDetailsBinding
         get() = FragmentPersonalTodoDetailsBinding::inflate
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun setUp() {
-        TODO("Not yet implemented")
+        val bundle = this.arguments
+        val details = bundle!!.getParcelable(DETAILS_PARAM, PersonalTodo::class.java)
     }
 
     override fun addCallBacks() {
         TODO("Not yet implemented")
+    }
+
+    private companion object {
+        private const val DETAILS_PARAM = "personal todo details"
+        fun newInstance(details: PersonalTodo) =
+            PersonalTODODetails().apply {
+                arguments = Bundle().apply {
+                    putParcelable(DETAILS_PARAM, details)
+                }
+            }
     }
 }
