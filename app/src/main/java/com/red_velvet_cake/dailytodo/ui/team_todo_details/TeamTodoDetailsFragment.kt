2542887abcb16b7e07b220c3d8d4042c338cdedc
Mohammed Alhams <mohammed1212alhams@gmail.com>
2023-04-20 04:18:31 +0200

    unifying update status spinners

--- app/src/main/java/com/red_velvet_cake/dailytodo/ui/team_todo_details/TeamTodoDetailsFragment.kt
+++ app/src/main/java/com/red_velvet_cake/dailytodo/ui/team_todo_details/TeamTodoDetailsFragment.kt
@@ -12,24 +12,27 @@ import com.red_velvet_cake.dailytodo.data.model.TeamTodo
 import com.red_velvet_cake.dailytodo.databinding.FragmentTeamTodoDetailsBinding
 import com.red_velvet_cake.dailytodo.ui.base.BaseFragment
 import com.red_velvet_cake.dailytodo.utils.Constants
-import com.red_velvet_cake.dailytodo.utils.TodoStatus
 import com.red_velvet_cake.dailytodo.utils.navigateBack
 
 class TeamTodoDetailsFragment : BaseFragment<FragmentTeamTodoDetailsBinding>(),
-    TeamTodoDetailsView {
+    AdapterView.OnItemSelectedListener, TeamTodoDetailsView {
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentTeamTodoDetailsBinding
         get() = FragmentTeamTodoDetailsBinding::inflate
 
     private val presenter by lazy { TeamTodoStatusPresenter(this) }
+    private lateinit var teamTodoDetails: TeamTodo
 
     override fun setUp() {
+        setArgs()
         initSpinner()
         getTeamTodoFromArguments()
     }
 
-    private fun initSpinner() {
-        val teamTodoDetails: TeamTodo? = arguments?.getParcelable(KEY_DETAILS_PARAM)
+    private fun setArgs() {
+        teamTodoDetails = arguments?.getParcelable(KEY_DETAILS_PARAM)!!
+    }
 
+    private fun initSpinner() {
         val status = listOf(
             getString(R.string.todo_status),
             getString(R.string.in_progress_status),
@@ -37,42 +40,11 @@ class TeamTodoDetailsFragment : BaseFragment<FragmentTeamTodoDetailsBinding>(),
         )
         val spinnerAdapter =
             ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, status)
-        spinnerAdapter.setDropDownViewResource(android.R.layout.select_dialog_multichoice)
+        spinnerAdapter.setDropDownViewResource(R.layout.item_todo_type)
 
         binding.spinnerTodoStatus.apply {
             adapter = spinnerAdapter
-            onItemSelectedListener =
-                object : AdapterView.OnItemSelectedListener {
-                    override fun onItemSelected(
-                        parent: AdapterView<*>?,
-                        view: View?,
-                        position: Int,
-                        id: Long
-                    ) {
-                        when (position) {
-                            Constants.TODO -> {
-                                presenter.setTodoStatus(TodoStatus.Todo, teamTodoDetails?.id!!)
-                            }
-
-                            Constants.IN_PROGRESS -> {
-                                presenter.setTodoStatus(
-                                    TodoStatus.InProgress,
-                                    teamTodoDetails?.id!!
-                                )
-                            }
-
-                            Constants.DONE -> {
-                                presenter.setTodoStatus(TodoStatus.Done, teamTodoDetails?.id!!)
-                            }
-                        }
-                    }
-
-                    override fun onNothingSelected(parent: AdapterView<*>?) {
-                        Toast.makeText(requireContext(), "Nothing Selected ", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-
-                }
+            onItemSelectedListener = this@TeamTodoDetailsFragment
         }
     }
 
@@ -112,5 +84,29 @@ class TeamTodoDetailsFragment : BaseFragment<FragmentTeamTodoDetailsBinding>(),
                 putParcelable(KEY_DETAILS_PARAM, teamTodo)
             }
         }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when (position) {
+            Constants.TODO -> {
+                presenter.updateTeamTodoStatus(Constants.TODO, teamTodoDetails.id)
+            }
+
+            Constants.IN_PROGRESS -> {
+                presenter.updateTeamTodoStatus(
+                    Constants.IN_PROGRESS,
+                    teamTodoDetails.id
+                )
+            }
+
+            Constants.DONE -> {
+                presenter.updateTeamTodoStatus(Constants.DONE, teamTodoDetails.id)
+            }
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        Toast.makeText(requireContext(), "Nothing Selected ", Toast.LENGTH_SHORT)
+            .show()
     }
 }
--- app/src/main/java/com/red_velvet_cake/dailytodo/ui/team_todo_details/TeamTodoStatusPresenter.kt
+++ app/src/main/java/com/red_velvet_cake/dailytodo/ui/team_todo_details/TeamTodoStatusPresenter.kt
@@ -1,23 +1,12 @@
 package com.red_velvet_cake.dailytodo.ui.team_todo_details
 
 import com.red_velvet_cake.dailytodo.data.remote.todo_service.TodoServiceImpl
-import com.red_velvet_cake.dailytodo.utils.Constants
-import com.red_velvet_cake.dailytodo.utils.TodoStatus
 
 class TeamTodoStatusPresenter(
     private val view: TeamTodoDetailsView,
 ) {
     private val apiService = TodoServiceImpl()
-
-    fun setTodoStatus(status: TodoStatus, todoId: String) {
-        when (status) {
-            TodoStatus.Todo -> updateTeamTodoStatus(Constants.TODO, todoId)
-            TodoStatus.InProgress -> updateTeamTodoStatus(Constants.IN_PROGRESS, todoId)
-            TodoStatus.Done -> updateTeamTodoStatus(Constants.DONE, todoId)
-        }
-    }
-
-    private fun updateTeamTodoStatus(
+    fun updateTeamTodoStatus(
         newTodoStatus: Int,
         todoId: String,
     ) {
