9424e92a3094cf1e1f7417971026306fc6091b8e
Mohammed Alhams <mohammed1212alhams@gmail.com>
2023-04-20 05:02:00 +0200

    enhance dependency inversion

--- app/src/main/java/com/red_velvet_cake/dailytodo/data/remote/todo_service/AuthorizationInterceptor.kt
+++ app/src/main/java/com/red_velvet_cake/dailytodo/data/remote/todo_service/AuthorizationInterceptor.kt
@@ -1,15 +1,17 @@
 package com.red_velvet_cake.dailytodo.data.remote.todo_service
 
-import com.red_velvet_cake.dailytodo.data.local.SharedPrefs
+import com.red_velvet_cake.dailytodo.data.local.LocalDataImpl
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
 class AuthorizationInterceptor : Interceptor {
+
+    private val localDataImpl = LocalDataImpl()
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
 
-        val token = SharedPrefs.token
+        val token = localDataImpl.getUserToken()
         val authHeaderValue = "$BEARER $token"
         val newRequest: Request = request.newBuilder()
             .addHeader(HEADER_AUTHORIZATION, authHeaderValue)
