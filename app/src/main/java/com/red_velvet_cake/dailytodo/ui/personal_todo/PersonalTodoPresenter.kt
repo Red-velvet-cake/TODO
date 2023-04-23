340e16b85c2d7340f5f37bc0761662792931da99
ibalzaidi <iaz@hippodata.net>
2023-04-23 19:39:28 +0200

    adding search feature to personaltodo & teamtodo using RxJava.

--- app/src/main/java/com/red_velvet_cake/dailytodo/ui/personal_todo/PersonalTodoPresenter.kt
+++ app/src/main/java/com/red_velvet_cake/dailytodo/ui/personal_todo/PersonalTodoPresenter.kt
@@ -4,10 +4,16 @@ import com.red_velvet_cake.dailytodo.data.model.GetAllPersonalTodosResponse
 import com.red_velvet_cake.dailytodo.data.model.PersonalTodo
 import com.red_velvet_cake.dailytodo.data.remote.todo_service.TodoServiceImpl
 import com.red_velvet_cake.dailytodo.data.remote.util.CustomException
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class PersonalTodoPresenter(private val view: PersonalTodoView) {
 
     private val todoServiceImpl = TodoServiceImpl()
+    private val compositeDisposable = CompositeDisposable()
 
     fun updateTeamTodoStatus(
         todoId: String,
@@ -59,5 +65,32 @@ class PersonalTodoPresenter(private val view: PersonalTodoView) {
 
     fun navigateToTodoDetails(personalTodo: PersonalTodo) {
         view.navigateToTodoDetails(personalTodo)
+    }
+
+    fun searchTodos(searchQuery: String) {
+        compositeDisposable.clear()
+
+        if (searchQuery.isBlank()) {
+            getAllTeamTodos()
+        } else {
+            val disposable = Observable.just(searchQuery)
+                .debounce(500, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .map { query ->
+                    view.getDisplayedTodoList().filter {
+                        it.title.contains(query, ignoreCase = true) ||
+                                it.description.contains(query, ignoreCase = true)
+                    }
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { filteredList ->
+                    view.showTodoList(filteredList)
+                }
+            compositeDisposable.add(disposable)
+        }
+    }
+
+    fun onDestroy() {
+        compositeDisposable.dispose()
     }
 }
